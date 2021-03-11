9fd2bb91bfff75f5f1d43322e8af05f23bb16cbb
Thomas Stehle <thomasstehle@Thomass-iMac.fritz.box>
2021-03-11 17:57:45 +0100

    Changed OpenGL version to ES 300

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 #include <SDL.h>
 
 #define GLES_SILENCE_DEPRECATION
-#include <OpenGLES/ES2/gl.h>
-
-#include <iostream>
+#include <OpenGLES/ES3/gl.h>
 
 namespace {
 void printProgramLog(GLuint f_programId) {
@@ -16,7 +14,7 @@ void printProgramLog(GLuint f_programId) {
     int infoLogLen = 0;
     glGetProgramInfoLog(f_programId, logLen, &infoLogLen, infoLog_a);
 
-    std::cout << infoLog_a << std::endl;
+    SDL_Log("%s\n", infoLog_a);
     delete[] infoLog_a;
   }
 }
@@ -30,7 +28,7 @@ void printShaderLog(GLuint f_shaderId) {
     int infoLogLen = 0;
     glGetShaderInfoLog(f_shaderId, logLen, &infoLogLen, infoLog_a);
 
-    std::cout << infoLog_a << std::endl;
+    SDL_Log("%s\n", infoLog_a);
     delete[] infoLog_a;
   }
 }
@@ -87,53 +85,65 @@ GLuint loadProgram(const GLchar* f_vertSource_p, const GLchar* f_fragSource_p) {
 int main(int, char**) {
   // Init SDL
   if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
-    std::cerr << SDL_GetError() << std::endl;
+    SDL_Log("%s\n", SDL_GetError());
     return EXIT_FAILURE;
   }
 
   SDL_version version;
   SDL_GetVersion(&version);
-  std::cout << "SDL version: " << static_cast<int>(version.major) << "."
-            << static_cast<int>(version.minor) << "."
-            << static_cast<int>(version.patch) << std::endl;
+  SDL_Log("SDL version: %d.%d.%d\n", static_cast<int>(version.major), static_cast<int>(version.minor), static_cast<int>(version.patch));
   
   // Create window
   SDL_SetHint(SDL_HINT_OPENGL_ES_DRIVER, "1");
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_EGL, 1);
   SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_ES);
-  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 2);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+  SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
 
   // Explicitly set channel depths, otherwise we might get some < 8
   SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
   SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
-  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 0);
+  SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
   SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
   const auto windowFlags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_FULLSCREEN_DESKTOP | SDL_WINDOW_BORDERLESS;
   auto window = SDL_CreateWindow(nullptr, 0, 0, 0, 0, windowFlags);
   if (!window) {
-      std::cerr << "Could not initialize Window" << std::endl;
+    SDL_Log("%s\n", SDL_GetError());
       return EXIT_FAILURE;
   }
 
   // Init GL
   auto glContext = SDL_GL_CreateContext(window);
   SDL_GL_MakeCurrent(window, glContext);
-  std::cout << "GL version: " << glGetString(GL_VERSION) << std::endl;
+  SDL_Log("GL version: %s\n", glGetString(GL_VERSION));
+    
+  // Set viewport
+  int w = 0;
+  int h = 0;
+  SDL_GL_GetDrawableSize(SDL_GL_GetCurrentWindow(), &w, &h);
+  glViewport(0, 0, w, h);
 
   // Load shader program
   constexpr char kVS[] = R"(
-    attribute vec4 vPosition;
+    #version 300 es
+    layout(location = 0) in vec2 a_position;
+    layout(location = 1) in vec2 a_texCoords;
+    out vec2 v_texCoords;
     void main() {
-      gl_Position = vPosition;
+      v_texCoords = a_texCoords;
+      gl_Position = vec4(a_position, 0, 1);
     })";
 
   constexpr char kFS[] = R"(
+    #version 300 es
     precision mediump float;
+    in vec2 v_texCoords;
+    out vec4 out_color;
     void main() {
-      gl_FragColor = vec4(gl_FragCoord.x / 512.0, gl_FragCoord.y / 512.0, 0.0, 0.1);
+      out_color = vec4(v_texCoords.x, v_texCoords.y, 0.0, 1.0);
     })";
 
   auto program = loadProgram(kVS, kFS);
@@ -145,31 +155,20 @@ int main(int, char**) {
     while (0 != SDL_PollEvent(&event)) {
       if (SDL_QUIT == event.type) {
         isRunning = false;
-      } else if (SDL_KEYDOWN == event.type) {
-        const auto keyStates_p = SDL_GetKeyboardState(nullptr);
-        if (keyStates_p[SDL_SCANCODE_ESCAPE]) {
-          isRunning = false;
-        }
       }
     }
     
     // Clear
     glClearColor(0.2F, 0.2F, 0.2F, 1.F);
     glClear(GL_COLOR_BUFFER_BIT);
-    
-    // Set viewport
-    int w = 0;
-    int h = 0;
-    SDL_GL_GetDrawableSize(SDL_GL_GetCurrentWindow(), &w, &h);
-    glViewport(0, 0, w, h);
 
     // Render scene
-    GLfloat vertices[] = {
-        0.0f, 0.5f, 0.0f, -0.5f, -0.5f, 0.0f, 0.5f, -0.5f, 0.0f,
-    };
+    const GLfloat coords[] = { 0.f, 0.5f, -1.f, -0.5f, 1.f, -0.5f };
     glUseProgram(program);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, coords);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, coords);
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     // Update window
